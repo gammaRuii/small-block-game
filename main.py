0db4549236a8828630e02741b07ebd660d785f29
gammaRuii <xruichen0411@gmail.com>
2022-08-03 18:18:43 -0700

    add buttons, darken buttons when pressed, resize layout and add a gray background

--- main.py
+++ main.py
@@ -1,20 +1,26 @@
 import kivy
+from kivy.metrics import dp
 from kivy.uix.gridlayout import GridLayout
 from kivy.app import App
 from kivy.uix.button import Button
 from tenByTengrid import BigGrid
 from random import randint
 from kivy.uix.boxlayout import BoxLayout
+from kivy.core.window import Window
 
+Window.clearcolor = (.5,.5,.5,0.7)
 
 class BigGrid(GridLayout):
     def __init__(self,**kwargs):
         super().__init__(**kwargs)
+        self.size_hint = (None,None)
+        self.size = (dp(1200),dp(800))
         self.cols = 10
-        colorList = [(1, 0, 0, 0.7), (0, 1, 0, 0.7), (0, 0, 1, 0.7)]
+
         print("bess")
         for i in range(100):
-            b = Button(background_normal = '', background_color = colorList[randint(0,2)])
+            color = randint(3,10)
+            b = Button(background_normal = 'snapshot0{}.png'.format(color), background_down = 'snap0{}d.png'.format(color))
             self.add_widget(b)
 
 class GameApp(App):
