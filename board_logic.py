b1a098352998d578b666b091c199259d1767f591
Qian Chen <qian.chen@bytedance.com>
2022-08-03 23:31:22 -0700

    add  templates for next step & remove unnecessary files

---
+++ board_logic.py
@@ -0,0 +1,57 @@
+
+
+class BoardLogic:
+    def __init__(self, size):
+        # the core data is an array to save the ball status on the board
+        # initially, it is all empty, with all values set to 0
+        # the length of the array is size * size, as the 
+        board = []
+        for i in range(size):
+            line = []
+            for j in range(size):
+                line.append(0)
+            board.append(line)
+
+        # here we have an initialized all-zero board data 
+        self.boardData = board
+
+
+    # this function is the response to the action that the player put a ball in a given location
+    # what is the logic you can think of?
+    def putBallInSpot(self, col, row, ball_val):
+        '''
+        A possible logic flow
+
+        if self.IsValidAction(...) == false :
+            throw an exception or return an error to indicate it
+
+        set the corresponding element in the array = ball_val
+
+        Detect if there are any lines, vertical/horizontal/diagonal, containing 5 or more same-value elements.
+        If yes, remove them by re-setting these elements to zero.
+
+        if self.IsGameOver(self):
+            throw an exception or return an error to indicate it
+
+        return
+        '''
+        pass
+
+    # this function return the boardData array
+    def GetBoardData(self):
+        return self.boardData
+
+    # this function check whether the board is in a game-over state
+    def IsGameOver(self):
+        pass
+
+    # this function detects if there are any lines, vertical/horizontal/diagonal, containing 5 or more same-value elements.
+
+    ## Question: Do you think this function is a good design? Is it efficient for our use case?
+    ## can u think of a better one?
+    def ClearConnectedLines(self):
+        pass
+        
+
+
+
--- main.py
+++ main.py
@@ -3,34 +3,75 @@ from kivy.metrics import dp
 from kivy.uix.gridlayout import GridLayout
 from kivy.app import App
 from kivy.uix.button import Button
-from tenByTengrid import BigGrid
+#from tenByTengrid import BigGrid
 from random import randint
 from kivy.uix.boxlayout import BoxLayout
 from kivy.core.window import Window
 
+from board_logic import BoardLogic
+
 Window.clearcolor = (.5,.5,.5,0.7)
+board_size = 7
+default_size = 10
+
+### please think what behavior each widget should have? What callbacks needs to be defined?
+### and what data will be updated in each callback?
 
 class BigGrid(GridLayout):
-    def __init__(self,**kwargs):
+    def __init__(self, size, TwoDArray, **kwargs):
         super().__init__(**kwargs)
         self.size_hint = (None,None)
-        self.size = (dp(1200),dp(800))
-        self.cols = 10
+        self.size = (dp(800),dp(800))
+        if size <= 0:
+            size = default_size
+        self.cols = size
 
-        print("bess")
-        for i in range(100):
-            color = randint(3,10)
-            b = Button(background_normal = 'snapshot0{}.png'.format(color), background_down = 'snap0{}d.png'.format(color))
-            self.add_widget(b)
+        self.draw(size, TwoDArray)
+
+
+    # we need to call the function every time the board status (a new ball placed, or a new game started)
+    # please make sure it can redraw 
+    def draw(self, size, TwoDArray):
+        for i in range(size):
+            for j in range(size):
+                color = TwoDArray[i][j]
+                # for each and every button below, we need to find a way to let it send back the column & row number
+                if color < 3 or color > 10 :
+                    b = Button()
+                else :
+                    b = Button(background_normal = 'snapshot0{}.png'.format(color), background_down = 'snap0{}d.png'.format(color))
+                self.add_widget(b)
+
+# Where the score is displayed
+class ScoreBoard:
+    pass
+
+# display three balls for player to place
+# once selected and placed, the correspong spot will be disabled. 
+class NextRoundBallPicker:
+    pass
+
+# once clicked, the game will be reset, a new game starts
+class RestartButton:
+    pass
+
+
 
 class GameApp(App):
     def __init__(self,**kwargs):
         super().__init__(**kwargs)
+        self.boardData = BoardLogic(board_size)
+        self.nextBallColor = 0
+        self.nextBallLocation = [0, 0]
+        self.score = 0
 
     def build(self):
-        return BigGrid()
+        # we need to return a screen with the board of grids, next-round ball picker, scoreboard, "Start" Buttone, etc
+        return BigGrid(board_size, self.boardData.GetBoardData())
+
 
 
 app = GameApp()
 
 app.run()
+
